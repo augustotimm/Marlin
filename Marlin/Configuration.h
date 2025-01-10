6ff7eab52e39e8b20877bb3b09647403a865e94e
timm <augusto.tes@gmail.com>
2025-01-09 23:57:23 -0300

    changes based on reddit tutorial

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -24,7 +24,7 @@
 #define CONFIG_EXAMPLES_DIR "BIQU/B1 - SKR 2"
 
 // Enable this option for BLTouch support
-//#define B1_USE_BLTOUCH
+#define B1_USE_BLTOUCH
 
 /**
  * Configuration.h
@@ -1314,7 +1314,7 @@
 //#define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
@@ -1920,12 +1920,12 @@
  */
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
-//#define AUTO_BED_LEVELING_UBL
-#ifdef B1_USE_BLTOUCH
-  #define AUTO_BED_LEVELING_BILINEAR
-#else
-  #define MESH_BED_LEVELING
-#endif
+#define AUTO_BED_LEVELING_UBL
+// #ifdef B1_USE_BLTOUCH
+//   #define AUTO_BED_LEVELING_BILINEAR
+// #else
+//   #define MESH_BED_LEVELING
+// #endif
 
 /**
  * Commands to execute at the end of G29 probing.
@@ -2140,7 +2140,7 @@
  * - Allows Z homing only when XY positions are known and trusted.
  * - If stepper drivers sleep, XY homing may be required again before Z homing.
  */
-//#define Z_SAFE_HOMING
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // (mm) X point for Z homing
